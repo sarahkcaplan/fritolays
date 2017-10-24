d7866d0c874173a7eb1b4c34e63721fe36863990
Sarah Caplan <caplan.sarah@gmail.com>
2017-10-23 20:54:47 -0700

    Add infowindows on click

--- public/js/application.js
+++ public/js/application.js
@@ -1,47 +1,36 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
 
 var locations =[
-{lat: 33.9416, lng: -118.4085}, //lax
-{lat:22.3186, lng:114.1796}, //hk
-{lat: 18.7061, lng: 98.9817}, //chiang mai
-{lat: 19.0760, lng: 72.8777}, //mumbai
-{lat: -26.2041, lng: 28.0473}, //johannesberg
-{lat: -34.6037, lng: -58.3816}, //buenos aires
-{lat: 40.7128, lng: -74.0059} //nyc
+  {lat: 33.9416, lng: -118.4085, name: 'LAX', picture: '../images/laxlays.jpg'},
+  {lat:22.3186, lng:114.1796, name: 'Hong Kong', picture: '../images/hklays.jpg'},
+  {lat: 18.7061, lng: 98.9817, name: 'Chiang Mai', picture: '../images/thailandlays.jpg'},
+  {lat: 19.0760, lng: 72.8777, name: 'Mumbai', picture: '../images/mumbailays.jpg'},
+  {lat: -26.2041, lng: 28.0473, name:'Johannesberg', picture: '../images/laxlays.jpg'},
+  {lat: -34.6037, lng: -58.3816, name: 'Buenos Aires', picture: '../images/argentinalays.jpg'},
+  {lat: 40.7128, lng: -74.0059, name: 'NYC', picture: '../images/newyorklays.jpg'}
 ]
-
 
 function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 3,
-  center: {lat:22.3186, lng:114.1796}
-});
-
-  var markers = locations.map(function(location) {
-      return new google.maps.Marker({
-        position: location,
-        map : map
-    });
+    zoom: 1,
+    center: {lat:22.3186, lng:114.1796}
   });
 
-
-  var mumbai = {lat: 19.0760, lng: 72.8777};
-  var contentString = 'String on a marker';
-  var infowindow = new google.maps.InfoWindow({
-        content: contentString
+  function markerPicture(map, marker, contentString) {
+    return function(){
+      var infowindow = new google.maps.InfoWindow({
+      content: contentString
+        });
+      infowindow.open(map, marker); };
+    }
+  for(city in locations){
+    var latLng = {lat: locations[city].lat, lng: locations[city].lng};
+    var contentString = locations[city].picture;
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map : map
       });
-
-  var marker = new google.maps.Marker({
-        position: mumbai,
-        map: map,
-        title: 'Mumbai'
-      });
-      marker.addListener('click', function() {
-        infowindow.open(map, marker);
-      });
+    marker.addListener('click', markerPicture(map, marker, contentString))
+  }
 }
